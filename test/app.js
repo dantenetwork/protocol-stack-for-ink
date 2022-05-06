32262631ad11943696aa5dff921b1be19dd41daf
xiyu1984 <shanellyy@126.com>
2022-05-06 16:35:11 +0800

    test passed

--- test/app.js
+++ test/app.js
@@ -10,16 +10,33 @@ const keyring = new Keyring({ type: 'sr25519' });
 let data = fs.readFileSync('./.secret/keyPair.json');
 const sender = keyring.addFromJson(JSON.parse(data.toString()));
 sender.decodePkcs8(process.env.PASSWORD);
+// console.log(sender)
 
 
-const abiFile = fs.readFileSync('./abi/metadata.json');
+const abiFile = fs.readFileSync('./abi/protocol.json');
 const contract = new ContractPromise(api, JSON.parse(abiFile), process.env.CONTRACT_ADDRESS);
 
-const myABI = new Abi(JSON.parse(abiFile));
-// console.log(myABI.messages);
-const encodedInfo = myABI.findMessage('submit_message');
-console.log(encodedInfo);
-console.log(encodedInfo.toU8a([{"name": "Nika", "age": 36, "phones": ["123", "456"]}]));
+// const myABI = new Abi(JSON.parse(abiFile));
+// // console.log(myABI.messages);
+// const encodedInfo = myABI.findMessage('submit_message');
+// console.log(encodedInfo);
+// console.log(encodedInfo.toU8a([{"name": "Nika", "age": 18, "phones": ["123", "456"]}]));
+
+const calleeFile = fs.readFileSync('./abi/callee.json');
+const calleeABI = new Abi(JSON.parse(calleeFile));
+const calleeEncode = calleeABI.findMessage('encode_user_defined_struct').toU8a([{"name": "Nika", "age": 18, "phones": ["123", "456"]}]);
+// console.log(calleeABI.findMessage('encode_user_defined_struct'));
+
+// const ecdStr = Array.prototype.map.call(calleeEncode, (x) => ('00' + x.toString(16).slice(-2)));
+let ecdStr = '0x';
+for (let i = 1; i < calleeEncode.length; ++i){
+  let stemp = calleeEncode[i].toString(16);
+  if (stemp.length < 2){
+    stemp = '0' + stemp;
+  }
+  ecdStr += stemp;
+}
+// console.log(ecdStr);
 
 // Read from the contract via an RPC call
 async function query() {
@@ -31,8 +48,13 @@ async function query() {
   
   // Perform the actual read (with one param, which is an user defined struct)
   // (We perform the send from an account, here using address created from a Json)
-  const { gasConsumed, result, output } = await contract.query['submitMessage'](sender.address, { value, gasLimit }, 
-                                          {"name": "Nika", "age": 36, "phones": ["123", "456"]});
+  // const { gasConsumed, result, output } = await contract.query['submitMessage'](sender.address, { value, gasLimit }, 
+  //                                         {"name": "Nika", "age": 18, "phones": ["123", "456"]});
+
+  // const calleeEncode = calleeABI.findMessage('encode_user_defined_struct').toU8a([{"name": "Nika", "age": 18, "phones": ["123", "456"]}]);
+  // 0x104e696b6112000000080c3132330c343536
+  const { gasConsumed, result, output } = await contract.query['callToContracts'](sender.address, {value, gasLimit }, 
+                                          "5EFwGBXG6DwPJUVoNPCtBPhqVtXqb9sPzZrtXdSp2A1T31VZ", ecdStr);
   
   // The actual result from RPC as `ContractExecResult`
   console.log(result.toHuman());
