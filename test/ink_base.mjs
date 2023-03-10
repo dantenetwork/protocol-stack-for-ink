cf52b86b608af10c724f454d5a966382472f52dc
xiyu1984 <shanellyy@126.com>
2023-03-10 18:20:26 +0800

    fixed weight V2

--- test/ink_base.mjs
+++ test/ink_base.mjs
@@ -1,20 +1,31 @@
 import {ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { Abi, ContractPromise } from '@polkadot/api-contract';
 import { decodeAddress, encodeAddress } from '@polkadot/keyring';
+// import { WeightV2 } from '@polkadot/types/interfaces';
 
-import BN from 'bn.js'
+import { BN, BN_ONE } from "@polkadot/util";
 import fs from 'fs'
 
+const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
+const PROOFSIZE = new BN(1_000_000);
+
 // sign and send transaction
-export async function sendTransaction(gasLimit, contract, methodName, sender, args) {
+export async function sendTransaction(api, contract, methodName, sender, args) {
     try {
         let value = 0;
-        const options = { storageDepositLimit: null, gasLimit }
+        const options = { 
+            storageDepositLimit: null, 
+            gasLimit: api.registry.createType('WeightV2', {
+                refTime: MAX_CALL_WEIGHT,
+                proofSize: PROOFSIZE,
+            }),
+        };
+
         const { gasRequired, storageDeposit, result } = await contract.query[methodName](
             sender.address,
             options,
             ...args
-            );
+        );
         console.log('gasRequired', gasRequired.toString());
         await contract.tx[methodName]({ value, gasLimit: gasRequired }, ...args).signAndSend(sender, (result) => {
             console.log(result.toHuman());
@@ -33,10 +44,19 @@ export async function sendTransaction(gasLimit, contract, methodName, sender, ar
 }
 
 // query info from blockchain node
-export async function contractCall(gasLimit, contract, method, from, args) {
+export async function contractCall(api, contract, method, from, args) {
     let value = 0;
 
-    const { gasConsumed, result, output } = await contract.query[method](from, {value, gasLimit}, ...args);
+    const { gasConsumed, result, output } = await contract.query[method](from, 
+        {
+            value, 
+            gasLimit: api.registry.createType('WeightV2', {
+                refTime: MAX_CALL_WEIGHT,
+                proofSize: PROOFSIZE,
+            }),
+            storageDepositLimit: null
+        }, 
+        ...args);
 
     return {gasConsumed, result, output};
 }
@@ -60,21 +80,11 @@ export class InkBase {
 
     // sign and send transaction
     async sendTransaction(methodName, ...args) {
-        const gasLimit = this.api.registry.createType("WeightV2", {
-            refTime: new BN("10000000000"),
-            proofSize: new BN("10000000000"),
-        });
-
-        return await sendTransaction(gasLimit, this.contract, methodName, this.devSender, args);
+        return await sendTransaction(this.api, this.contract, methodName, this.devSender, args);
     }
 
     // query info from blockchain node
     async contractCall(method, ...args) {
-        const gasLimit = this.api.registry.createType("WeightV2", {
-            refTime: new BN("10000000000"),
-            proofSize: new BN("10000000000"),
-        });
-
-        return await contractCall(gasLimit, this.contract, method, this.devSender.address, args);
+        return await contractCall(this.api, this.contract, method, this.devSender.address, args);
     }
 }
